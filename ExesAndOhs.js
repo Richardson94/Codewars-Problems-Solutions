91d1b203220a62d8417a8f354d0e44d833de5444
Nelson Richard Cori Sirpa <Richard.Cori@fundacion-jala.org>
2022-10-24 19:37:51 -0400

    adding solution of kata's problem about count X or O character

---
+++ ExesAndOhs.js
@@ -0,0 +1,24 @@
+function XO(str) {
+  var countX = 0;
+  var countO = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == 'x' || str[i] == 'X') {
+      countX++;
+    }
+    if (str[i] == 'o' || str[i] == 'O') {
+      countO++;
+    }
+  }
+  return countO == countX ? true : false;
+}
+console.log(XO('xo'));
+console.log(XO('xxOo'));
+console.log(XO('xxxm'));
+console.log(XO('Oo'));
+console.log(XO('ooom'));
+/*
+ x ->120
+ X ->88
+ o -> 111
+ O -> 79
+ */
