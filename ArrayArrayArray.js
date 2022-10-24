20f2474822f49633b22fb3f6cf839c432b4236f6
Nelson Richard Cori Sirpa <Richard.Cori@fundacion-jala.org>
2022-10-24 19:25:04 -0400

    solution kata's about generate array of array inside values

---
+++ ArrayArrayArray.js
@@ -0,0 +1,19 @@
+function explode(x) {
+  if (x[0] != parseInt(x[0]) && x[1] != parseInt(x[1])) {
+    return 'Void!';
+  } else {
+    var newArray = [];
+    if (x[0] == parseInt(x[0])) {
+      for (let i = 1; i <= x[0]; i++) {
+        newArray.push(x);
+      }
+    }
+    if (x[1] == parseInt(x[1])) {
+      for (let i = 1; i <= x[1]; i++) {
+        newArray.push(x);
+      }
+    }
+    return newArray;
+  }
+}
+console.log(explode(['a', 3]));
